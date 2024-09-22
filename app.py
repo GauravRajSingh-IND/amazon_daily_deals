8002a9b45c333fe4586e09ab61b80bfc75fccf90
Gaurav Raj Singh <grsmanohar@gmail.com>
2024-09-22 16:52:40 +0530

    add a background image

--- app.py
+++ app.py
@@ -7,9 +7,17 @@ class UI:
         self.window = tkinter.Tk()
         self.window.title("Amazon Australia Deals Newsletter")
         self.window.geometry("1000x800")
-        self.window.config(bg= "gray20")
 
-        # add Icon
+        # create a canvas.
+        self.canvas = tkinter.Canvas(self.window, bg= "gray20", width=1000, height=800, highlightthickness= 0)
+
+        # add amazon icon.
+        self.icon_path = tkinter.PhotoImage(file="Images/amazon_icon.png")
+        self.canvas.create_image(500, 400, image= self.icon_path)
+
+        self.canvas.place(x=0, y=0)
+
+
 
     def exit(self):
         self.window.mainloop()
