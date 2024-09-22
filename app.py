b7fd305f6f279112ba5f74354a0291bc1bbe52eb
Gaurav Raj Singh <grsmanohar@gmail.com>
2024-09-22 17:01:02 +0530

    add labels and title

--- app.py
+++ app.py
@@ -15,6 +15,20 @@ class UI:
         self.icon_path = tkinter.PhotoImage(file="Images/amazon_icon.png")
         self.canvas.create_image(500, 400, image= self.icon_path)
 
+        # add text.
+        self.canvas.create_text(500, 100, text= "Amazon Newsletter", font= ('arial', 30, 'bold'), fill= "snow",
+                                anchor= "center")
+        self.canvas.create_text(200, 200, text= "username : ", font= ('arial', 30, 'bold'), fill= "snow",
+                                anchor= "w")
+        self.canvas.create_text(200, 270, text="Email : ", font=('arial', 30, 'bold'), fill="snow",
+                                anchor="w")
+        self.canvas.create_text(200, 340, text="number : ", font=('arial', 30, 'bold'), fill="snow",
+                                anchor="w")
+        self.canvas.create_text(200, 410, text="category : ", font=('arial', 30, 'bold'), fill="snow",
+                                anchor="w")
+        self.canvas.create_text(200, 480, text="No. Deals : ", font=('arial', 30, 'bold'), fill="snow",
+                                anchor="w")
+
         self.canvas.place(x=0, y=0)
 
 
