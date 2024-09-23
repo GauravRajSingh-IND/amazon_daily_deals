fd28d8d310cb233c8bc1a0876ce9f558a2379588
Gaurav Raj Singh <grsmanohar@gmail.com>
2024-09-23 18:47:03 +0530

    create a function to add customer data

--- api.py
+++ api.py
@@ -35,3 +35,41 @@ class API_FETCH:
             return {"success":True, "response":response.json()}
         except requests.RequestException as e:
             return {"success":False, "response":f"error: {e}"}
+
+
+class SheetyUpdate:
+
+    def __init__(self):
+
+        self.end_point = os.getenv('sheety_url')
+        self.bear_token = os.getenv('sheety_BEARER_TOKEN')
+
+    def add_customer(self, email:str, username:str, phone_number:int, category:int, number_of_deals:int = 10) -> dict:
+
+        # Check - all the values are not None.
+        if not all([email, username, phone_number, category, number_of_deals]):
+            return {"success":False, "response":"Please fill all the required values."}
+
+        header = {
+            "Authorization": f"Bearer {self.bear_token}"
+        }
+
+        customer_data = {
+                        "customer" : {
+                                        "email":email,
+                                        "username":username,
+                                        "phonenumber":phone_number,
+                                        "category":category,
+                                        "numberdeals":number_of_deals,
+                                      }
+                        }
+
+        try:
+            response = requests.post(url=self.end_point,json=customer_data, headers=header)
+            response.raise_for_status()
+            return {"success":True, "response":"Customer Added"}
+        except requests.RequestException as e:
+            return {"success": False, "response": f"error: {e}"}
+
+
+
